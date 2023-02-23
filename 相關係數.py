4cfcebe32b3fbeab10c5db1e5d0511143d224274
abcd334 <123975061+abcd334@users.noreply.github.com>
2023-02-23 23:27:02 +0800

    20230223

--- 相關係數.py
+++ 相關係數.py
@@ -10,24 +10,26 @@ def Corr(data,corr_limit):
 
         df_stock = pd.read_csv(os.path.join(data_folder, filename), index_col='Date', parse_dates=True)
         df_returns[symbol] = df_stock['Close']
-        print(symbol)
+
 
     # 計算相關係數
     corr = df_returns.corr()
-    groups = []
 
-    while len(corr) > 0:
-        code = corr.index[0]
-        group = corr[code][corr[code] < corr_limit].index
-        groups.append(group)
-        corr.drop(index=group, columns=group, inplace=True)
+    groups = pd.DataFrame()
+    
+    for symbol in data['Code']:
+        symbol_corr=corr.loc[symbol]
+        for code in symbol_corr.index:
+            if symbol_corr.loc[code] < corr_limit:
+                groups=groups.append({'symbol': symbol, 'code': code}, ignore_index=True)
 
-    # 输出分组
-    for i, group in enumerate(groups):
-        return(f"Group {i+1}: {list(group)}")
+    with pd.ExcelWriter('output.xlsx', mode='w') as writer:
+        groups.to_excel(writer, sheet_name='sheet1', index=False, startrow=0, startcol=0, header=True)
+
+    return groups
 
 if __name__ == '__main__':
     data = pd.read_excel('Product Code.xlsx')
     data_folder = 'History Data'
-    result = Corr(data,0.1)
+    result = Corr(data,0.5)
     print(result)
