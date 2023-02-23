32e66e36ccb831ad390463f48c14891fbafd60fe
abcd334 <123975061+abcd334@users.noreply.github.com>
2023-02-23 14:14:12 +0800

    20230223

--- 抓取歷史股價.py
+++ 抓取歷史股價.py
@@ -1,9 +1,11 @@
 import yfinance as yf
-import pandas as pd
-import openpyxl
+import os
 
-df = yf.Ticker("^TWII").history(period="max")
-data = pd.ExcelWriter('TSEA.xlsx',engine='openpyxl',mode='a')
-data.to_excel(writer, sheet_name='工作表1', index=False)
+ticker_code='^TWII'
+df = yf.Ticker(ticker_code).history(period="max")
 
-writer.save()
+if os.path.exists(ticker_code+'.csv'):
+    os.remove(ticker_code+'.csv')
+
+with open(ticker_code+'.csv', mode='a',newline='') as file:
+    df.to_csv(file, index=True, header=not file.tell())
